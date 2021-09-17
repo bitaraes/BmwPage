ccd5d14b9a93213d74db27e88501969428cf4dc2
Igor <igor_oliveira02@hotmail.com>
2021-09-17 00:38:44 -0300

    add footer and home alterations

---
+++ src/components/shared/cars-card/cars-card.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Button from "../button/button";
+
+import "./style.css";
+
+export default function CarsCard({ img, name }) {
+	return (
+		<div className="cars-card">
+			<img src={`${img}`} alt="Foto do veículo" />
+			<h3>{name}</h3>
+			<div className="card-button">
+				<Button color="blue" text="Cotação" />
+			</div>
+			<div className="social">
+				<span>Compartilhe: </span>
+				<i className="icon-facebook"></i>
+				<i className="icon-whatsapp"></i>
+			</div>
+		</div>
+	);
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,17 @@
 import React from "react";
+import Banner from "../components/banner/banner";
+import Footer from "../components/footer/footer";
 
 import Header from "../components/header/header";
+import ListCars from "../components/list-cars/list-cars";
 
 export function Home() {
-	return <Header />;
+	return (
+		<>
+			<Header />
+			<Banner />
+			<ListCars />
+			<Footer />
+		</>
+	);
 }
